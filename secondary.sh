be65a1005c2bd1da2fac8a6be28776b7dc055635
Paulo <pauloamino76@gmail.com>
2025-09-03 14:14:58 -0300

    feat: Adicionando hoppscotch

--- secondary.sh
+++ secondary.sh
@@ -13627,7 +13627,7 @@ exibir_menu() {
     OPCOES[62]="Hoppscotch"
 
     local pagina1_items=(1 2 3 4 6 7 8 9 10 13 14 15 16 17 18 19 20 21 22 23 24 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 43 43 44 45)
-    local pagina2_items=(46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61)
+    local pagina2_items=(46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62)
     local pagina_atual=1
 
     while true; do
