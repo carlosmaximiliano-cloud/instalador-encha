bfa688331ad1314aaada8750fd60202507d335ec
Paulo <pauloamino76@gmail.com>
2025-09-03 15:40:17 -0300

    feat: Adicionando WPPconnect arrumado

--- secondary.sh
+++ secondary.sh
@@ -13039,7 +13039,7 @@ networks:
     external: true
 EOL
 
-  STACK_NAME= "browserless${1:+_$1}"
+  STACK_NAME="browserless${1:+_$1}"
   stack_editavel
 
   echo -e "\e[97m• VERIFICANDO SERVIÇO \e[33m[3/3]\e[0m"
@@ -14192,9 +14192,10 @@ exibir_menu() {
     OPCOES[63]="Bolt"
     OPCOES[64]="Planka"
     OPCOES[65]="WPPconnect"
+    OPCOES[66]="Browserless"
 
     local pagina1_items=(1 2 3 4 6 7 8 9 10 13 14 15 16 17 18 19 20 21 22 23 24 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 43 43 44 45)
-    local pagina2_items=(46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65)
+    local pagina2_items=(46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66)
     local pagina_atual=1
 
     while true; do
