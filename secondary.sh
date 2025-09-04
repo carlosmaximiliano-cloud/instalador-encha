e31a8b2d3487daeaad257ae6bae5f2800f280384
Paulo <pauloamino76@gmail.com>
2025-09-04 03:02:04 -0300

    refactor: menu

--- secondary.sh
+++ secondary.sh
@@ -14,9 +14,9 @@ roxo="\033[35m"
 azul="\033[34m"
 branco="\e[1;37m"   # Branco brilhante
 reset="\033[0m"
-amarelo="\e[33m"
-verde="\e[32m"
-vermelho="\e[31m"
+amarelo="\033[33m"
+verde="\033[32m"
+vermelho="\033[31m"
 banner(){
   clear
   clear
